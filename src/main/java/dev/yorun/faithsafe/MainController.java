a5956573870e677be4d24bebe7228cfad50a2c79
BambusTM <schmamina@gmail.com>
2024-05-20 11:51:02 +0200

    added listview for paswords

--- src/main/java/dev/yorun/faithsafe/MainController.java
+++ src/main/java/dev/yorun/faithsafe/MainController.java
@@ -1,10 +1,29 @@
 package dev.yorun.faithsafe;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 
-public class MainController {
-    @FXML
-    Stage stage;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class MainController implements javafx.fxml.Initializable {
+    @FXML
+    private ListView<String> pwListView;
+
+    @FXML
+    private Label pwPreviewLabel;
+
+    String[] passwords = {"password1", "password2", "password3"};
+    String currentPassword;
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        pwListView.getItems().addAll(passwords);
+        pwListView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            currentPassword = newValue;
+            pwPreviewLabel.setText(currentPassword);
+        });
+    }
 }
