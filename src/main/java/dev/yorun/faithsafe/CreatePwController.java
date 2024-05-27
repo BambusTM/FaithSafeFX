870a865ae397a1c35c96eec0fcd1268398bf67ab
RoBaertschi <rtmbaertschi007@gmail.com>
2024-05-27 14:26:05 +0200

    fix shit

--- src/main/java/dev/yorun/faithsafe/CreatePwController.java
+++ src/main/java/dev/yorun/faithsafe/CreatePwController.java
@@ -51,7 +51,7 @@ public class CreatePwController {
         String description = createPwDescription.getText();
 
         if (confirmPw()) {
-            jsonMapper.saveToJson(DATA_PATH, username, domain, email, password, description);
+            jsonMapper.saveToJson(username, domain, email, password, description);
 
             if (onPasswordCreated != null) {
                 onPasswordCreated.accept(null);
