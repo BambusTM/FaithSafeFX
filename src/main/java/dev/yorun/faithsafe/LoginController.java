4da1bd77b13a69e955c28cb922f3ad5655e8018b
BambusTM <81088539+BambusTM@users.noreply.github.com>
2024-05-28 19:26:05 +0200

    Able to switch to reset scene and use the abort button to go back to login

--- src/main/java/dev/yorun/faithsafe/LoginController.java
+++ src/main/java/dev/yorun/faithsafe/LoginController.java
@@ -4,9 +4,11 @@ import static dev.yorun.faithsafe.service.Variables.USER_PATH;
 
 import dev.yorun.faithsafe.service.JsonMapper;
 import dev.yorun.faithsafe.service.StageService;
+import java.util.Objects;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -24,12 +26,6 @@ public class LoginController {
   @FXML
   private Button resetButton;
 
-  private Scene loginScene;
-
-  @FXML
-  public void initialize() {
-    loginScene = loginButton.getScene();
-  }
 
   public void login() {
     checkPassword();
@@ -37,14 +33,11 @@ public class LoginController {
 
   public void switchToResetPwView(ActionEvent event) {
     try {
-      FXMLLoader loader = new FXMLLoader(getClass().getResource("login-reset-view.fxml"));
-      Parent root = loader.load();
-      ResetPwController resetPwController = loader.getController();
-      resetPwController.setPreviousScene(loginScene);
-      Stage currentStage = (Stage) loginButton.getScene().getWindow();
+      Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("login-reset-view.fxml")));
+      Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
       Scene scene = new Scene(root);
-      currentStage.setScene(scene);
-      currentStage.setTitle("Reset Password");
+      stage.setScene(scene);
+      stage.show();
     } catch (Exception e) {
       System.err.println("Failed to switch to login-reset-view.fxml: " + e.getMessage());
     }
